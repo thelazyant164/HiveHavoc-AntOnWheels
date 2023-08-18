fc42d775702984ce727a5d155a3c57b40d995a65
thelazyant164 <lythienan164@gmail.com>
2023-08-18 10:13:39 +1000

    Add environment system interface

---
+++ Assets/Script/System/Environment/IPoisonous.cs
@@ -0,0 +1,11 @@
+using Com.StillFiveAsianStudios.HiveHavocAntOnWheels.Combat;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Com.StillFiveAsianStudios.HiveHavocAntOnWheels.Environment
+{
+    public interface IPoisonous : IDamaging
+    {
+    }
+}
